a17faba0cad4a7c9dddadae1f299f2cf79fe71dd
lloosky <karolswitalla@gmail.com>
2023-04-04 00:13:57 +0200

    added pg connection

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const pg = require("pg");
+const pool = new pg.Pool({
+    user: "postgres",
+    host: "127.0.0.1",
+    database: "postgres",
+    password: "admin",
+    port: "5432"
+});
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    pool.query("SELECT * FROM users", (err, result) => {
+        console.log(err, result);
+        res.send(result.rows)
+        pool.end();
+    });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
